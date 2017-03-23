2fda397f95fd5aabbbc7546d44f81222b4e45488
fengyy51 <694590074@qq.com>
2017-03-23 17:30:18 +0800

    尝试控制按键颜色变化

--- js/tizhitest.js
+++ js/tizhitest.js
@@ -1,3 +1,15 @@
+//九种体质对应数据数组
+var yangxuzhi=["阳虚",7];
+var yinxuzhi=[8];
+var qixuzhi=["气虚",8];
+var tanshizhi=[8];
+var shirezhi=[7];
+var xueyuzhi=[7];
+var telinzhi=[7];
+var qiyuzhi=["qiyuuzhi",7];
+var pinghezhi=["平和",8];
+//保存上次点击按键的name值
+var pos=0;
 $(document).ready(function(){
 	$('#btn-sex-male').click(function(){
 		$('#real-title-tizhi').hide();
@@ -14,48 +26,72 @@ $(document).ready(function(){
 
 });
 function action(){
-	// if($('div.question[value]').attr("class").is("active"))
-	// {
-	// 	$('div.question[value]').click(function(){
-	// 		$('div.question[value]')
-	// 	})
-	// }
-	$('.num').click(function(){
-		var i=$(this).attr('name');
+		
+	$('.btn').click(function(event){
+		var i=$(this).parents('.question').attr("name");
+		var me=$(this);
+		
+		// 单击后添加背景色
+			// this.style.backgroundColor="rgba(33,158,78,0.5)";
+			$(this).addClass('addcolor');
+
+		// 单击此行按键后方才显示下一行
 		var j=parseInt(i)+1;
 		$('#no-'+j).removeClass('disable');
 		$('#no-'+j).addClass('active');
-		console.log(j);	
+
+		if($(this).parents('.pinghezhi').length){
+				var i=$(this).attr("name");
+				pos=parseInt(i);	
+				pinghezhi.push(i);
+				console.log(pinghezhi);
+				$(this).parents('.question').addClass('true');
+			};
+		//判断所属体质类别，将对应数值添加到相应类别的数组中
+			if($(this).parents('.qixuzhi').length){
+				var i=$(this).attr("name");
+				pos=parseInt(i);
+				qixuzhi.push(i);
+				console.log(qixuzhi);
+				flag=true;
+				$(this).parents('.question').addClass('true');
+			};
+
+
+		//点击同一行两遍时只显示第二次的值，第一次的按键不显示
+		if($(this).parents('.true').length){
+		
+			// console.log(pos);
+			// var $beforebutton=$(".btn[name="+pos+"]");
+			// $beforebutton.css("backgroundColor","rgba(255,255,255,0.5)");
+			// console.log($(this).parent().parent().find($(".btn[name="+pos+"]")));
+
+			
+			if($(this).parents('.pinghezhi').length){
+				// $(this).parents('.btn-group-justified').find($(".btn[name="+pos+"]")).css("backgroundColor","rgba(255,255,255,0.5)");
+				$(this).parents('.btn-group-justified').find($('.btn')).removeClass('addcolor');
+				$(this).addClass('addcolor');
+				pinghezhi.splice($.inArray(pos,pinghezhi),1)
+				// console.log(pinghezhi);
+			}
+			if($(this).parents('.qixuzhi').length){
+				$(this).parents('.btn-group-justified').find($(".btn[name="+pos+"]")).css("backgroundColor","rgba(255,255,255,0.5)");
+
+				qixuzhi.splice($.inArray(pos,qixuzhi),1)
+				console.log(qixuzhi);
+			}
+
+		}
+		
+			
+			//判断所属体质类别，将对应数值添加到相应类别的数组中
+			
+	
+		
 	});
-	// $('#no-'+1).click(function(){
-	// 	// $('#real-title-tizhi').hide();
-	// 	$('#no-'+2).removeClass('disable');
-	// 	$('#no-'+2).addClass('active');
-	// 	action();
-	// });
-	// for(var k=0;k<100;k++){
-	// 	$("div:has('.active')").click(function() {
-	// 		var i=this.attr('value');
-	// 		var j=i+1;
-	// 		$('#no-'+j).removeClass('disable');
-	// 		$('#no-'+j).addClass('active');
-	// 		console.log(j);	
-	// });
-	// }
+
 	
-	// var i=$('div.num')
 	
-	// for(var i=1;i<=60;i++){
-	// 	var j=i+1;
-	// 	if($('#no-'+i).hasClass('active')){
-	// 		$('#no-'+i).click(function(){
-	// 		$('#no-'+j).removeClass('disable');
-	// 		$('#no-'+j).addClass('active');
-	// 		console.log(j);
-	// 	})
-	// 	}
-		
-		// }
-		
+
 	
 }
