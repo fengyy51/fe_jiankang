8e928d668e6c9596c359d5a5f51dccf0307c814c
owen05 <liuhongnan53@163.com>
2017-04-06 15:26:52 +0800

    fix

--- js/personal_information.js
+++ js/personal_information.js
@@ -1,82 +1,81 @@
-$(document).ready(function(){
-	var currentUrl = window.location.href ;
-	currentUrl = currentUrl.split("/") ;
-	currentUrl.pop() ;
-	currentUrl = currentUrl.join("/") ;
-	var state = getQueryString("state") ;
-	if (state != null) {
-		var code = getQueryString("code") ;
-		var data = {"code" : code} ;
-		//Ajax(data , "post" , severAddress + "/user/do-auth" , 
-		Ajax(data , "post" , severAddress + "/user/test" , 
-			function(data) {
-				/*var data = JSON.parse(data) ;
-				var headurl = data.data.headImgUrl ;
-					loadDom(headurl) ;*/
-				//window.location.href = "http://huzhu.liuhongnan.com/page/personal_information.html" ;
-				window.location.href = uri + "/page/personal_information.html" ;
-			
-			}
-			, true
-		) ;
-	} else {
-		var data = {} ;
-		Ajax(data , "post" , severAddress + "/user/get-wechat-info" ,
-			function(data) {
-				var data = JSON.parse(data) ;
-				if (data.isAuth == false) {
-					//var https = encodeURI("https://huzhu.liuhongnan.com/page/personal_information.html") ;
-					//window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx63d2b1530c8bb787&redirect_uri=http%3a%2f%2fhuzhu.liuhongnan.com%2fpage%2fpersonal_information.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect" ;
-					window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx63d2b1530c8bb787&redirect_uri=https%3a%2f%2fhuzhu.liuhongnan.com%2fpage%2fpersonal_information.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect" ;
-				} else if(data.isLogin == false) {
-					//window.location.href = "http://huzhu.liuhongnan.com/page/sign_up.html" ;
-					window.location.href = uri + "/page/sign_up.html" ;
-				} else {
-					var headurl = data.data.headImgUrl ;
-					loadDom(headurl) ;
-				}
-			}
-			,true
-		) ;
-	}
+$(document).ready(function() {
+    var currentUrl = window.location.href;
+    currentUrl = currentUrl.split("/");
+    currentUrl.pop();
+    currentUrl = currentUrl.join("/");
+    var state = getQueryString("state");
+    if (state != null) {
+        var code = getQueryString("code");
+        var data = { "code": code };
+        Ajax(data, "post", severAddress + "/user/do-auth",
+            function(data) {
+                var data = JSON.parse(data);
+                var headurl = data.data.headImgUrl;
+                loadDom(headurl);
+                //window.location.href = "http://huzhu.liuhongnan.com/page/personal_information.html" ;
+                // window.location.href = uri + "/page/personal_information.html" ;
 
-	//load dom
-	function loadDom(headurl) {
-		var children ;
-		children = '<img src="' + headurl + '" width="100%" display:"block" height="550px">' ;
-		$("#headPicture").html(children) ;
+            }, true
+        );
+    } else {
+        var data = {};
+        Ajax(data, "post", severAddress + "/user/get-wechat-info",
+            function(data) {
+                var data = JSON.parse(data);
+                if (data.isAuth == false) {
+                    // alert("123");
+                    //var https = encodeURI("https://huzhu.liuhongnan.com/page/personal_information.html") ;
+                    window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx63d2b1530c8bb787&redirect_uri=http%3a%2f%2fhuzhu.liuhongnan.com%2fpage%2fpersonal_information.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
+                    //window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx63d2b1530c8bb787&redirect_uri=https%3a%2f%2fhuzhu.liuhongnan.com%2fpage%2fpersonal_information.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
+                } else if (data.isLogin == false) {
+                    //window.location.href = "http://huzhu.liuhongnan.com/page/sign_up.html" ;
+                    window.location.href = uri + "/page/sign_up.html";
+                } else {
+                    // alert("yes!!!!!!!");
+                    var headurl = data.data.headImgUrl;
+                    loadDom(headurl);
+                }
+            }, true
+        );
+    }
 
-		//个人信息
-		s = [] ;
-		var imgAddr = ["../resource/img/account.png","../resource/img/image-text.png","../resource/img/machinery.png"] ;
-		s.push('<div class="am-list-body">') ;
-		var children ;
-		var content = ["&nbsp&nbsp我的资料管理","&nbsp&nbsp我的电子凭证","&nbsp&nbsp我的收货地址"] ;
-		var addr = [currentUrl + "/personal_information_manage.html",currentUrl + "/electronic_certificate.html", currentUrl + "/shop_address.html"] ;
-		children = addListAddr(content,imgAddr,addr) ;
-		s.push(children) ;
-		s.push('<div class="am-whitespace ws10px"></div>') ;
-					
-					
-		//健康模块
-		var imgAddr = ["../resource/img/保障.png","../resource/img/cart.png"] ;
-		var children ;
-		var content = ["&nbsp&nbsp体质测评","&nbsp&nbsp爱康商城"] ;
-		var addr = [currentUrl + "/tizhi.html",currentUrl + "/shopping.html"] ;
-		children = addListAddr(content,imgAddr,addr) ;
-		s.push(children) ;
-		s.push('<div class="am-whitespace ws10px"></div>') ;
-					
-		//区块链
-		var imgAddr = ["../resource/img/积分.png","../resource/img/卡包.png","../resource/img/pic.png"] ;
-		var children ;
-		var content = ["&nbsp&nbsp积分管理","&nbsp&nbsp电子钱包","&nbsp&nbsp区块链公示"] ;
-		var addr = [currentUrl + "/jifen.html",currentUrl + "/money.html", currentUrl + "/gongshi.html"] ;
-		children = addListAddr(content,imgAddr,addr) ;
-		s.push(children) ;
-					
-		s.push('</div>') ;
+    //load dom
+    function loadDom(headurl) {
+        var children;
+        children = '<img src="' + headurl + '" width="100%" display:"block" height="550px">';
+        $("#headPicture").html(children);
 
-		$("#personalInformation").html( s.join('') ) ;
-	}
-}) ;
+        //个人信息
+        s = [];
+        var imgAddr = ["../resource/img/account.png", "../resource/img/image-text.png", "../resource/img/machinery.png"];
+        s.push('<div class="am-list-body">');
+        var children;
+        var content = ["&nbsp&nbsp我的资料管理", "&nbsp&nbsp我的电子凭证", "&nbsp&nbsp我的收货地址"];
+        var addr = [currentUrl + "/personal_information_manage.html", currentUrl + "/electronic_certificate.html", currentUrl + "/shop_address.html"];
+        children = addListAddr(content, imgAddr, addr);
+        s.push(children);
+        s.push('<div class="am-whitespace ws10px"></div>');
+
+
+        //健康模块
+        var imgAddr = ["../resource/img/保障.png", "../resource/img/cart.png"];
+        var children;
+        var content = ["&nbsp&nbsp体质测评", "&nbsp&nbsp爱康商城"];
+        var addr = [currentUrl + "/tizhi.html", currentUrl + "/shopping.html"];
+        children = addListAddr(content, imgAddr, addr);
+        s.push(children);
+        s.push('<div class="am-whitespace ws10px"></div>');
+
+        //区块链
+        var imgAddr = ["../resource/img/积分.png", "../resource/img/卡包.png", "../resource/img/pic.png"];
+        var children;
+        var content = ["&nbsp&nbsp积分管理", "&nbsp&nbsp电子钱包", "&nbsp&nbsp区块链公示"];
+        var addr = [currentUrl + "/jifen.html", currentUrl + "/money.html", currentUrl + "/gongshi.html"];
+        children = addListAddr(content, imgAddr, addr);
+        s.push(children);
+
+        s.push('</div>');
+
+        $("#personalInformation").html(s.join(''));
+    }
+});
--- js/util/global.js
+++ js/util/global.js
@@ -1,73 +1,143 @@
-severAddress = "https://123.57.37.50:8081" ;
-//severAddress = "http://127.0.0.1:8080" ;
-//severAddress = "http://169.254.212.67:8081" ;
-appid = 'wx63d2b1530c8bb787' ;
-redirect_uri = encodeURI("https://huzhu.liuhongnan.com") ;
-uri = "https://huzhu.liuhongnan.com" ;
+severAddress ="http://123.57.37.50:8081";
+//severAddress ="http://127.0.0.1:8080";
+//severAddress ="http://169.254.212.67:8081";
+appid ='wx63d2b1530c8bb787';
+redirect_uri = encodeURI("http: //huzhu.liuhongnan.com");
+uri ="http://huzhu.liuhongnan.com";
 
-function Ajax(postData , method , url , f) {
-	var query = "" ;
-	var cou = 0 ;
+function Ajax(postData, method, url, f) {
+    var query ="";
+    var cou = 0;
 
-	for (var prop in postData) {
-		if (cou == 0) {
-			query += prop + "=" + postData[prop] ;
-		} else {
-			query += "&" + prop + "=" + postData[prop] ;
-		}
-	}
+    for (var prop in postData) {
+        if (cou == 0) {
+            query += prop + "=" + postData[prop];
+        }
+        else {
+            query += "&" + prop + "=" + postData[prop];
+        }
+    }
+    if (method == "get") {
+        if (query != "") {
+            query ="?"+ query;
+            url += query;
+        }
+    }
+    var xhr = new loadXMLDoc();
 
-	if (method == "get") {
-		if (query != "") {
-			query = "?" + query ;
-			url += query ;
-		}
-	}
+    if (arguments.length == 5) {
+        xhr.withCredentials = arguments[4];
+    }
+    xhr.open(method, url, true);
+    xhr.responseType ="text";
 
-	var xhr = new loadXMLDoc() ;
-	xhr.responseType = "text" ;
-	if (arguments.length == 5) {
-		xhr.withCredentials = arguments[4] ;
-	}
-	xhr.open(method , url , true) ;
-	xhr.onreadystatechange = function(){
-	    var XMLHttpReq = xhr ;
-	    if (XMLHttpReq.readyState == 4) {
-	        if (XMLHttpReq.status == 200 || XMLHttpReq.status == 304) {
-	            var text = XMLHttpReq.responseText ;
-	            f(text) ;
-	        }
-	    }
-	} ;
-	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	if (method == "post") {
-		if (query == "") {
-			xhr.send() ;
-		} else {
-			xhr.send(query) ;
-		}
-	} else {
-		xhr.send() ;
-	}
+    xhr.onreadystatechange = function() {
+        var XMLHttpReq = xhr;
+
+        if (XMLHttpReq.readyState == 4) {
+
+            if (XMLHttpReq.status == 200 || XMLHttpReq.status == 304) {
+                var text = XMLHttpReq.responseText;
+                f(text);
+            }
+        }
+    }
+    ;
+
+    xhr.onerror = function(e) {
+        alert("error!")
+    }
+
+    if (method == "post") {
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        if (query == "") {
+            xhr.send();
+        }
+        else {
+            xhr.send(query);
+        }
+    }
+    else {
+        xhr.send();
+    }
 }
+function loadXMLDoc() {
+    var xmlhttp = null;
 
-function loadXMLDoc()
-{
-	var xmlhttp = null ;
-	if (window.XMLHttpRequest) {// code for all new browsers
-  		xmlhttp = new XMLHttpRequest();
-  	} else if (window.ActiveXObject) {// code for IE5 and IE6
-  		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  	}
-  	return xmlhttp ;
+    if (window.XMLHttpRequest) {
+        // code for all new browsers
+        xmlhttp = new XMLHttpRequest();
+    }
+    else if (window.ActiveXObject) {
+        // code for IE5 and IE6
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return xmlhttp;
 }
-
 function isAuth_isLogin(data) {
-	var data = JSON.parse(data) ;
-	if (data.isAuth == false) {
-		window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx63d2b1530c8bb787&redirect_uri=http%3a%2f%2fhuzhu.liuhongnan.com%2fpage%2fpersonal_information.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect" ;
+    var data = JSON.parse(data);
 
-	} else if(data.isLogin == false) {
-		window.location.href = uri + "/page/sign_up.html" ;
-	}
-}	
+    if (data.isAuth == false) {
+        window.location.href ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx63d2b1530c8bb787&redirect_uri=http%3a%2f%2fhuzhu.liuhongnan.com%2fpage%2fpersonal_information.html&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect";
+    }
+    else if (data.isLogin == false) {
+        window.location.href = uri + "/page/sign_up.html";
+    }
+}
+
+
+
+//新AJAX封装
+function AjaxNEW(type, url, data, success, withCredentials, failed) {
+    // 创建ajax对象
+    var xhr = null;
+
+    if(window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    }
+    else {
+        xhr = new ActiveXObject('Microsoft.XMLHTTP')
+    }
+    var type = type.toUpperCase();
+    // 用于清除缓存
+    var random = Math.random();
+
+    if(typeof data == 'object') {
+        var str ='';
+
+        for(var key in data) {
+            str += key+'='+data[key]+'&';
+        }
+        data = str.replace(/&$/, '');
+    }
+    xhr.withCredentials = withCredentials;
+    xhr.responseType ="text";
+
+    if(type == 'GET') {
+        if(data) {
+            xhr.open('GET', url + '?' + data, true);
+        }
+        else {
+            xhr.open('GET', url + '?t=' + random, true);
+        }
+        xhr.send();
+    }
+    else if(type == 'POST') {
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(data);
+    }
+    // 处理返回数据
+    xhr.onreadystatechange = function() {
+        if(xhr.readyState == 4) {
+            if(xhr.status == 200) {
+                success(xhr.responseText);
+            }
+            else {
+                if(failed) {
+                    failed(xhr.status);
+                }
+            }
+        }
+    }
+}
